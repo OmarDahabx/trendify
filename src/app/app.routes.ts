f4f16f9c929aa579c5564bdb26c280dc91684ea0
omar dahab <omar.dahabxx@gmail.com>
2025-08-16 20:52:45 +0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { NotFound } from './components/not-found/not-found';
 import { Games } from './components/trends/games/games';
 import { Home } from './components/home/home';
 import { Products } from './components/trends/products/products';
+import { About } from './components/about/about';
+import { Services } from './components/cservices/cservices';
+import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
     {path:'', redirectTo: 'home' , pathMatch:'full'},
@@ -14,5 +17,8 @@ export const routes: Routes = [
         {path:'products', component:Products },
         { path: 'games', component:Games }
     ]},
+    { path: 'about', component:About, title: 'About' },
+    { path: 'services', component:Services, title: 'Services' },
+    { path: 'contact', component:Contact , title: 'Contact' },
     {path:'**' , component:NotFound , title:'Not Found'}
 ];
